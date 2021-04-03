2e4222f9fc3646c2624f7dd843e6012976cfc37f
Sergio Toro Escofet <storoe1992@gmail.com>
2021-04-03 17:51:29 -0300

    1er commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList msg="Crea una nueva tarea"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoList
   }
 }
 </script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form class="tareaform">
+      <h2>Tarea</h2>
+      <input v-model="tarea" placeholder="Ingresa una nueva tarea"/>
+      <button @click="addTarea($event)">Crear</button>
+    </form>
+    <h3>Lista</h3>
+    <ul>
+      <li v-for="(tarear,index) in todolist" :key="index">{{tarear}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      todolist : [],
+      tarea : ""
+    }
+  },
+  methods : {
+    addTarea(event){
+      event.preventDefault();
+      if(!(this.tarea.trim() === "")){
+        this.todolist.push(this.tarea);
+        this.tarea = "";
+      }else{
+        alert("Escriba una tarea")
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" scoped>
+.tareaform{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  button{
+    margin-left: 5px;
+  }
+
+  h2 {
+    margin-right: 5px;
+  }
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
